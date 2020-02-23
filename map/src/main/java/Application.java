311eed9a2be44523be67754bfec17c7e4ec437b9
Summerdise <350464517@qq.com>
2020-02-24 03:03:30 +0800

    [zmc] feat: add addStudent function and complete application

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,19 @@
+import java.util.*;
+
 public class Application {
 
   public static void main(String[] args) {
+    Map<String, Integer> studentList = new HashMap<>();
+    studentList.put("Linda", 18);
+    studentList.put("Cindy", 19);
+    studentList.put("Mary", 19);
+    Teacher teacher = new Teacher("Bob", studentList);
+    teacher.addStudent(new Student("Ben", 19));
+    teacher.addStudent(new Student("Solider", 20));
+    for (String key : studentList.keySet()) {
+      String name = key;
+      int age = studentList.get(key);
+      System.out.println(key + "，今年" + age + "岁。");
+    }
   }
-
 }
--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,32 @@
-public class Teacher {
+import java.util.*;
 
+public class Teacher {
+    String name;
+    Map<String, Integer> studentList;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, Map<String, Integer> studentList) {
+        this.name = name;
+        this.studentList = studentList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Integer> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(Map<String, Integer> studentList) {
+        this.studentList = studentList;
+    }
+
+    public void addStudent(Student student) {
+        String name = student.getName();
+        int age = student.getAge();
+        this.studentList.put(name, age);
+    }
 }
